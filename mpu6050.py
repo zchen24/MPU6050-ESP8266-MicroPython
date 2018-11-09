f38faed1897b41e1b6f2e5bede3a3dd8dc090a60
Zihan Chen <zihan.chen.jhu@gmail.com>
2018-11-08 22:02:52 -0800

    Added set gyro/accel functions

--- mpu6050.py
+++ mpu6050.py
@@ -1,8 +1,10 @@
-import machine
+# MPU6050 Library for MicroPython
+
+from machine import I2C
 
 
 class accel():
-    def __init__(self, i2c, addr=0x68):
+    def __init__(self, i2c: I2C, addr=0x68):
         self.iic = i2c
         self.addr = addr
         self.iic.start()
@@ -39,6 +41,34 @@ class accel():
         vals["GyZ"] = self.bytes_toint(raw_ints[12], raw_ints[13])
         return vals  # returned in range of Int16
         # -32768 to 32767
+
+    def set_gyro_range(self, FS_SEL=0):
+        """ Set gyro full scale range. Ignore if FS_SEL is out of range.
+        Args:
+            FS_SEL (int): Full scale select.
+                0: +/-  250 deg/s
+                1: +/-  500 deg/s
+                2: +/- 1000 deg/s
+                3: +/- 2000 deg/s
+        """
+        if FS_SEL < 0 or FS_SEL > 3:
+            return
+        else:
+            self.iic.writeto_mem(self.addr, 0x1B, FS_SEL << 3)
+
+    def set_accel_range(self, AFS_SEL=0):
+        """ Set accelerometer full scale range. Ignore if AFS_SEL is out of range.
+        Args:
+            AFS_SEL (int): Full scale select.
+                0: +/- 2g
+                1: +/- 4g
+                2: +/- 8g
+                3: +/- 16g
+        """
+        if AFS_SEL < 0 or AFS_SEL > 3:
+            return
+        else:
+            self.iic.writeto_mem(self.addr, 0x1C, AFS_SEL << 3)
 
     def val_test(self):  # ONLY FOR TESTING! Also, fast reading sometimes crashes IIC
         from time import sleep
